b16e9222a1cf030dad07dbea79eb9e8d2f7e6a3a
emanuel <emanuel@DESKTOP-KQGNKSP>
2018-08-09 22:49:33 -0400

    testdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {INgxMyDpOptions, IMyDateModel} from 'ngx-mydatepicker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testdate';
+ 
+   constructor() { }
+  
+   onBlurMethod(){
+    alert("Oi");
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxMyDatePickerModule } from 'ngx-mydatepicker';
+import { TextMaskModule } from 'angular2-text-mask';
+import { SigDateComponent } from './sig-date/sig-date.component';  
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigDateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgxMyDatePickerModule.forRoot(), TextMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
